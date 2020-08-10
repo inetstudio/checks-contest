1442bf151bc3edc62bcf091737cce51d1ed56fd5
Konstantin Bukin <bukin@inetstudio.ru>
2020-08-10 10:15:36 +0300

    Fixes

--- entities/prizes/src/Http/Resources/Back/Resource/Show/ItemResource.php
+++ entities/prizes/src/Http/Resources/Back/Resource/Show/ItemResource.php
@@ -2,6 +2,7 @@
 
 namespace InetStudio\ReceiptsContest\Prizes\Http\Resources\Back\Resource\Show;
 
+use Carbon\Carbon;
 use Illuminate\Http\Resources\Json\JsonResource;
 use InetStudio\ReceiptsContest\Prizes\Contracts\Http\Resources\Back\Resource\Show\ItemResourceContract;
 
@@ -16,8 +17,8 @@ class ItemResource extends JsonResource implements ItemResourceContract
             'pivot' => $this->whenPivotLoaded('receipts_contest_receipts_prizes', function () {
                 return [
                     'confirmed' => $this['pivot']['confirmed'],
-                    'date_start' => $this['pivot']['date_start'],
-                    'date_end' => $this['pivot']['date_end'],
+                    'date_start' => $this['pivot']['date_start'] ? Carbon::parse($this['pivot']['date_start'])->format('Y-m-d\TH:i:s\Z') : null,
+                    'date_end' => $this['pivot']['date_end'] ? Carbon::parse($this['pivot']['date_end'])->format('Y-m-d\TH:i:s\Z') : null,
                 ];
             }),
         ];
